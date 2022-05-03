eb1b29147c61ca41bb0d32765a24a9834dfbd2e3
billy <billyh880217@gmail.com>
2022-05-04 00:23:01 +1200

    employee form

--- resources/js/components/employee/EmployeeForm.vue
+++ resources/js/components/employee/EmployeeForm.vue
@@ -1,25 +1,82 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-						Employee Form
-					</div>
+        <el-card class="box-card">
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+				<template #header>
+					<div class="card-header">
+						<el-page-header :content="`${scope} form`" @back="goBack" />
+					</div>
+				</template>
+				<div>
+					<el-form
+						ref="employeeForm"
+						:model="form"
+						label-width="120px"
+					>
+						<el-form-item label="Name" prop="name" required>
+							<el-input 
+								v-model="form.name"
+								placeholder="Employee Name"
+							></el-input>
+						</el-form-item>
+						<el-form-item label="Email" prop="email" required>
+							<el-input 
+								v-model="form.email"
+								type="email"
+								placeholder="Email Address"
+							></el-input>
+						</el-form-item>
+						<el-form-item label="Department" prop="department" required>
+							<el-input 
+								v-model="form.department"
+								placeholder="Department Name"
+							></el-input>
+						</el-form-item>
+
+						<el-form-item label="Section" prop="section" required>
+							<el-input 
+								v-model="form.section"
+								placeholder="Section Name"
+							></el-input>
+						</el-form-item>
+						
+						<el-form-item>
+							<el-button type="primary" @click="saveForm('employeeForm')">
+								Save
+							</el-button>
+						</el-form-item>
+						
+
+					</el-form>
+				</div>
+		</el-card>
     </div>
-</template>
+</template>	
 
 <script>
-    export default { 
-        mounted() {
-            console.log('Component mounted.')
-        }
+    export default {
+		name: 'employeeform',
+        props: {
+            scope: String,
+        },
+		data() {
+			return{
+				form: {
+					name: null,
+					email: null,
+					department: null,
+					section: null,
+				}
+			}
+		},
+		methods: {
+			goBack(){
+				window.location.href = "/employess";
+			},
+
+			saveForm(formName){
+				alert(formName)
+			}
+		}
     }
 </script>
